e34767ac47878f892dae5e8a44cf952a0a5606f8
John Furtado <jf8846@gmail.com>
2017-10-07 16:19:15 +0000

    My solution to HackerRank 30 days of code day 5

---
+++ loops.js
@@ -0,0 +1,12 @@
+function main() {
+    var n = parseInt(readLine());
+    var result;
+    var i = 1
+    
+    while (i <= 10) {
+      result = i * n;
+      console.log(n + " x " + i + " = " + result);
+      i++; 
+    };
+
+}
